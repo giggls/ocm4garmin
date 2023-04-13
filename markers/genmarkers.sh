17bdfd4e4e3302e86338af14959d104f4781d4ff
Sven Geggus <sven-git@geggus.net>
2023-04-13 21:56:29 +0200

    Manually fix antialiasing issues with bmp files (in source png)

--- markers/genmarkers.sh
+++ markers/genmarkers.sh
@@ -6,14 +6,16 @@
 #
 # directory containing markers from OpenCampingMap
 # https://github.com/giggls/opencampsitemap/tree/master/markers
+#
+# do not run this resulting files have been manually edited
+#
+
 DIR=~/osm/campsite-map/map/markers
 
 for bn in standard nudist backcountry camping caravan group_only; do
- inkscape --export-type=png --export-filename=markers/$bn.png --export-area-page --export-overwrite --export-background="rgb(255, 0, 255)" -h 24 $DIR/m_$bn.svg 2>/dev/null
- convert markers/$bn.png -compress none -type palette BMP3:markers/$bn.bmp;
- rm markers/$bn.png
- inkscape --export-type=png --export-filename=markers/${bn}_private.png --export-area-page --export-overwrite --export-background="rgb(255, 0, 255)" -h 24 $DIR/m_private_$bn.svg 2>/dev/null
- convert markers/${bn}_private.png -compress none -type palette BMP3:markers/${bn}_private.bmp;
- rm markers/${bn}_private.png
+ inkscape --export-type=png --export-filename=$bn.png --export-area-page --export-overwrite --export-background="rgb(255, 255, 255)" -h 24 $DIR/m_$bn.svg 2>/dev/null
+ convert $bn.png -compress none -type palette BMP3:$bn.bmp;
+ inkscape --export-type=png --export-filename=${bn}_private.png --export-area-page --export-overwrite --export-background="rgb(255, 255, 255)" -h 24 $DIR/m_private_$bn.svg 2>/dev/null
+ convert ${bn}_private.png -compress none -type palette BMP3:${bn}_private.bmp;
 done
 
